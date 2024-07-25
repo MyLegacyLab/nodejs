871f6b5a963c5669660d0a3f0eac4bbba53b705f
frtasoniero <frtasoniero@gmail.com>
2024-07-24 23:23:56 -0300

    Update test with fake repository

--- personal_blog_api/src/domain/use-cases/publish-article.spec.ts
+++ personal_blog_api/src/domain/use-cases/publish-article.spec.ts
@@ -1,15 +1,26 @@
 import { test, expect } from 'vitest';
 import { PublishArticleUseCase } from './publish-article';
+import { PublishArticleRepository } from '../repositories/publish-article-repository';
+import { Article } from '../entities/article';
 
-test('publish an article', () => {
-  const publishArticle = new PublishArticleUseCase();
+const fakePublishArticleRepository: PublishArticleRepository = {
+  create: async (article: Article) => {
+    return;
+  },
+};
+
+test('publish an article', async () => {
+  const publishArticle = new PublishArticleUseCase(
+    fakePublishArticleRepository,
+  );
 
   const title = 'Exploring TypeScript: A Modern JavaScript Alternative';
-  const content =
-    'TypeScript is a typed superset of JavaScript that compiles to plain JavaScript. It offers static type-checking along with the latest ECMAScript features. In this article, we explore the benefits of using TypeScript, its core features, and how it can improve your development workflow.';
+  const content = `TypeScript is a typed superset of JavaScript that compiles to plain JavaScript.
+    It offers static type-checking along with the latest ECMAScript features.
+    In this article, we explore the benefits of using TypeScript, its core features, and how it can improve your development workflow.`;
   const userId = 'user12345';
 
-  const article = publishArticle.execute({
+  const article = await publishArticle.execute({
     title: title,
     content: content,
     userId: userId,
