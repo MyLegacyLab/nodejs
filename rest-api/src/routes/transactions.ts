a4165f67a30e50bee7dbf39690a69e446ff4ed8b
frtasoniero <frtasoniero@gmail.com>
2024-08-03 23:43:22 -0300

    Add transactions routes and validation

--- rest-api/src/routes/transactions.ts
+++ rest-api/src/routes/transactions.ts
@@ -1,16 +1,52 @@
+import { FastifyInstance } from 'fastify';
 import { dbConn } from '../database';
-import crypto from 'node:crypto';
+import { z } from 'zod';
+import { randomUUID } from 'node:crypto';
 
-export async function transactionRoutes(app) {
-  app.post('/create-transaction', async () => {
-    const transaction = await dbConn('transactions')
-      .insert({
-        id: crypto.randomUUID(),
-        title: 'Testing transaction',
-        amount: 1000,
-      })
-      .returning('*');
+export async function transactionRoutes(app: FastifyInstance) {
+  app.get('/', async () => {
+    const transactions = await dbConn('transactions').select();
 
-    return transaction;
+    return { transactions };
+  });
+
+  app.get('/:id', async (request) => {
+    const getTransactionParamSchema = z.object({
+      id: z.string().uuid(),
+    });
+
+    const { id } = getTransactionParamSchema.parse(request.params);
+
+    const transaction = await dbConn('transactions').where('id', id).first();
+
+    return { transaction };
+  });
+
+  app.get('/summary', async () => {
+    const summary = await dbConn('transactions')
+      .sum('amount', { as: 'amount' })
+      .first();
+
+    return { summary };
+  });
+
+  app.post('/', async (request, reply) => {
+    const createTransactionBodySchema = z.object({
+      title: z.string(),
+      amount: z.number(),
+      type: z.enum(['income', 'expense']),
+    });
+
+    const { title, amount, type } = createTransactionBodySchema.parse(
+      request.body,
+    );
+
+    await dbConn('transactions').insert({
+      id: randomUUID(),
+      title: title,
+      amount: type === 'income' ? amount : amount * -1,
+    });
+
+    return reply.status(201).send();
   });
 }
